8bc213506721e758a885d8dd09b483ff2882d18c
Juan Suárez <suarezramirezjuanjose@hotmail.com>
2018-04-28 10:47:02 +0200

    Confirmo cambios

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ mongoose.connect('mongodb://localhost:27017/curso_mean2', (err, res) =>{
     console.log("La conexion a la base de datos, funciona");
 
     app.listen(port, function(){
-      console.log("Servidor del api rest de musica escuchando en http://localhost:", +port);
+      console.log("Servidor del api rest de musica que se puede escuchar en http://localhost:", +port);
     })
   }
 });
+
