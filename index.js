643900ce0d74124d8457e87131ecb64eb875f9bf
Juan Suárez <suarezramirezjuanjose@hotmail.com>
2018-04-28 10:49:26 +0200

    v2-comit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ mongoose.connect('mongodb://localhost:27017/curso_mean2', (err, res) =>{
   if(err){
     throw err;
   }else {
-    console.log("La conexion a la base de datos, funciona");
+    console.log("La conexión a la base de datos, funciona");
 
     app.listen(port, function(){
       console.log("Servidor del api rest de musica que se puede escuchar en http://localhost:", +port);
