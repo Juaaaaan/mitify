10a685d5431e15edb47c21f3e1a082660ff2a764
Juan Suárez <suarezramirezjuanjose@hotmail.com>
2018-04-28 12:20:16 +0200

    Más...

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 'use strict'
+var bcrypt = require('bcrpt-nodejs');
+var User = require('../models/user');
 
 function pruebas(req, res){
     res.status(200).send({
@@ -6,6 +8,27 @@ function pruebas(req, res){
     });
 }
 
+function saveUser(req, res){
+    var user = new User();
+
+    var params = req.body;
+
+    console.log(params);
+
+    user.name = params.name;
+    user.surname = params.surname;
+    user.email = params.email;
+    user.role = 'ROLE_USER';
+    user.image = 'null';
+
+    if(params.password){
+        // Encriptar contraseña y guardar los datos
+    }else{
+        res.status(500).send({message:'Introduzca la contraseña'});
+    }
+};
+
 module.exports = {
-    pruebas
+    pruebas,
+    saveUser
 };
