2cc6e89a6a5139e82047be118e491338e1a8260c
Juan Suárez <suarezramirezjuanjose@hotmail.com>
2018-05-06 12:22:06 +0200

    Vuelve a funcionar

--- controllers/artist.js
+++ controllers/artist.js
@@ -6,7 +6,7 @@ var path = require('path');
 
 //Utilizamos los siguientes modelos
 var Artist = require('../models/artist');
-var Album = require('../models/album');
+//var Album = require('../models/album');
 //var Song = require('../models/song');
 
 function getArtist(req, res){
