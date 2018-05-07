d8ade952b929184c4838fa9f20f80f756184c766
Juan Suárez <suarezramirezjuanjose@hotmail.com>
2018-05-07 12:10:36 +0200

    Eliminar artista + imagen artista + fallos de mongoose...
    Sigue fallando Mongoose en la linea 12 de artist.js -> Fallo de Schema...

--- controllers/artist.js
+++ controllers/artist.js
@@ -9,8 +9,8 @@ var mongoosePaginate = require('mongoose-pagination');
 
 //Utilizamos los siguientes modelos
 var Artist = require('../models/artist');
-//var Album = require('../models/album');
-//var Song = require('../models/song');
+var Album = require('../models/album');
+var Song = require('../models/song');
 
 //Get artist. Sacar artista de nuestra base de datos
 function getArtist(req, res){
@@ -74,6 +74,48 @@ function updateArtist(req, res){
     });
 }
 
+//Eliminar un artista
+    //Este metodo lo que lleva es, borrar al artista junto con todos sus album y borrar las canciones que tenga el album asociado
+
+function deleteArtist(req, res){
+    var artistId = req.params.id;
+
+    Artist.findByIdAndRemove(artistId, (err, artistRemoved) => {
+        if(err){
+            res.status(500).send({message: 'Error al eliminar el artista'});
+        }else{
+            if(!artistRemoved){
+                res.status(404).send({message: 'El artista no ha sido eliminado'});
+            }else{
+                //console.log(artistRemoved);
+                //Vamos a borrar todo lo que tenga asociado a él: 
+                    //Un where que saque todos los album cuyo artista sea eliminado por Id. Despues con el .remove, lo eliminamos
+                Album.find({artist:artistRemoved._id}).remove((err, albumRemoved)=> {
+                    if(err){
+                        res.status(500).send({message: 'Error al eliminar el álbum'});   
+                    }else{
+                        if(!albumRemoved){
+                            res.status(404).send({message: 'El álbum no ha sido eliminado'});
+                        }else{
+                            //Repetimos los mismos pasos, para la canción
+                            Song.find({artist:artistRemoved._id}).remove((err, songRemoved)=> {
+                                if(err){
+                                    res.status(500).send({message: 'Error al eliminar la canción'});   
+                                }else{
+                                    if(!songRemoved){
+                                        res.status(404).send({message: 'La canción no ha sido eliminada'});
+                                    }else{
+                                        res.status(200).send({artist: artistRemoved});
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        }
+    });
+}
 
 //Guardar artista
 function saveArtist(req, res){
@@ -97,12 +139,83 @@ function saveArtist(req, res){
     });
 }
 
+//Imagenes del artista
+
+function uploadImage(req, res){
+    var artistId = req.params.id;
+    var file_name = 'No subido...';
+
+    //Con el connect-multiparty podemos utilizar las variables superglobales files.
+        //Comprobaremos si viene algo por files
+        if(req.files){
+            //En caso de que el usuario haya subido una imagen
+            var file_name = req.file.image.path;
+            //La url de la imagen, nos sale todo el directorio entero, por tanto tenemos que recortar la url:
+            //Con el metodo SPLIT 
+            var file_split = file_path.file.split('\\');
+            //Recojo el nombre que este en la posición número 2
+            var file_name = file_split[2];
+    
+            //Si yo quiero sacar la extensión de la imagen:
+            var ext_split = file.name.path('\.');
+            //Recojo la extensión (.png, .jpg...)
+            var file_ext = ext_split[1];
+    
+            //Compruebo que la imagen tiene la extensión correcta
+            if(file_ext == 'png' || file_ext =='jpg' || file_ext == 'gif'){
+                //Hago un Find & Update para actualizar la imagen del usuario
+                Artist.findByIdAndUpdate(ArtistId, {image:file_name}, (err, ArtistUpdated) =>{
+                    if(!ArtistId){
+                        res.status(404).send({message: 'Se ha podido actualizar el artista'});
+                    }else{
+                        res.status(200).send({Artist: ArtistUpdated});
+                    }
+                });
+            }else{
+                res.status(404).send({message: 'Extensión del archivo no válida'});
+            }
+    
+            console.log(file_split);
+        }else{
+            //Si el usuario no ha subido ninguna imagen
+            res.status(404).send({message: 'No has subido ninguna imagen'});
+        }
+    }
+    
+    
+    //Metodo para que nos devuelva un fichero de imagen, es decir: le pasaremos el nombre de un fichero y nos lo devuelva directamente
+        //Nos haga una response (res) una respuesta http con el fichero tal cual, en este caso, una imagen
+        //Esto se hace para securizar lo ficheros del servidor porque siempre voy a pasar por un metodo de la api
+        //Y no vamos a saber la ruta de la imagen. Se accederá a bajo nivel y estará protegido con autentificación
+        //
+        //Para esto, tenemos que importar primeramente los modulos fs y path
+    
+        function getImageFile(req, res){
+            var imageFile = req.params.image_file;
+            var path_file = './uploads/artists/' + imageFile;
+            //Compruebo si existe un fichero en el servidor
+            fs.exists(path_file, function(exists){
+                if (exists) {
+                    res.sendFile(path_file);
+                }else{
+                    res.status(404).send({message: 'No existe la imagen...'});
+                }
+            });
+    
+        }
+    
+    
+
+
 
 //para poder utilizar los metodos que utilizar en nuestro controlador:
 module.exports = {
     getArtist,
     saveArtist, 
     getArtists,
-    updateArtist
+    updateArtist, 
+    deleteArtist, 
+    uploadImage,
+    getImageFile
 };
 
--- routes/artist.js
+++ routes/artist.js
@@ -7,11 +7,23 @@ var api = express.Router();
 //Cargamos el middleware de autentificación
 var md_auth = require('../middlewares/autenticated');
 
+//El Artista tendrá su imagen/avatar. Esto lo hacemos con Multiparty
+    //Esto lo que nos hace es poder enviar ficheros a través del protocolo http
+
+    var multipart = require('connect-multiparty');
+    //Crearemos un middleware.
+        //Dentro del multipart, tenemos que poner la ruta donde se van a subir los ficheros. 
+    var md_upload = multipart({ uploadDir: './uploads/artists' });
+    
+
 api.get('/artist:id', md_auth.ensureAuth, ArtistController.getArtist);
 api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist);
 //le pongo la interrogación porque puede que venga o puede que no venga
 api.get('/artist/:page?', md_auth.ensureAuth, ArtistController.getArtists);
 api.put('/artist/:id', md_auth.ensureAuth, ArtistController.updateArtist);
+api.delete('/artist/:id', md_auth.ensureAuth, ArtistController.deleteArtist);
+api.post('/upload-image-artist/:id', [md_auth.ensureAuth, md_upload], ArtistController.uploadImage);
+api.get('/get-image-artist/:imageFile', ArtistController.getImageFile);
 
 //Y exportamos los metodos del api
 module.exports = api;
